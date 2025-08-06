3b04d2ae73a0f186b615872801970c2d8567e9a4
HEran-ite <herieshetu@gmail.com>
2025-08-06 17:23:23 +0300

    Feat : initail commit-flutter and firebase setup

---
+++ lib/dashboard.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatelessWidget {
+  final String uid;
+  const DashboardPage({super.key, required this.uid});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
