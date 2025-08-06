70bdf0973d80aa1d1b62de7ad3120ac49964d309
HEran-ite <herieshetu@gmail.com>
2025-08-06 18:29:33 +0300

    Feat: fetch document from fire store in the dashboard

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,22 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class DashboardPage extends StatelessWidget {
   final String uid;
-  const DashboardPage({super.key, required this.uid});
-  
-  get username => null;
+  DashboardPage({super.key, required this.uid});
+
+  Future<String> getUserName() async {
+    final snapshot =
+        await FirebaseFirestore.instance
+            .collection("users")
+            .doc('test_user')
+            .get();
+    return snapshot.data()?['Name'] ?? '';
+  }
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title:Text('Dashboard'),
-      ),
+    final username = getUserName();
 
+    return Scaffold(
+      appBar: AppBar(title: Text('Dashboard')),
 
       body: Column(
         children: [
-          Text('Welcome $username')
+          Text('Welcome $username'),
+          ElevatedButton(onPressed: () {}, child: Text('update')),
         ],
       ),
     );
